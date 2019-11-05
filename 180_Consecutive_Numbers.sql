97878c003ac52a4b509c5f3b88869df2636be645
learnere <zhangpei1211@gmail.com>
2019-11-05 11:28:37 +0100

    solution

---
+++ 180_Consecutive_Numbers.sql
@@ -0,0 +1,4 @@
+SELECT distinct(tb1.Num) AS ConsecutiveNums
+FROM Logs AS tb1, Logs AS tb2, Logs AS tb3
+WHERE tb1.Id +1 = tb2.Id AND tb2.Id +1 = tb3.Id
+      AND tb1.Num = tb2.Num AND tb2.Num = tb3.Num;
