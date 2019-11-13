83f99a05127e55bcb485248aec4fb8a321ae0a4a
learnere <zhangpei1211@gmail.com>
2019-11-13 21:18:37 +0100

    update

--- 627_Swap_Salary.sql
+++ 627_Swap_Salary.sql
@@ -1,2 +1,8 @@
 UPDATE salary
 SET sex=(IF(sex = 'f','m','f'));
+SELECT (CASE  WHEN mod(id,2) = 1 AND id= counts THEN id
+              WHEN mod(id,2) = 1 AND id != counts THEN id+1
+              ELSE id-1
+        END) as id, student
+FROM seat, (SELECT count(*) as counts FROM seat) as counts
+ORDER BY id;
